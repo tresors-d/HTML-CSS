7135585e61e895fdfa5bac37a2aecd67842648ad
tresors-d <tresdongmo@gmail.com>
2023-10-22 16:52:05 +0100

    initialisation du projet et premier exercice

---
+++ js/test.js
@@ -0,0 +1,14 @@
+let a = 15;
+console.log(a);
+let b = "bonjour";
+console.log(b);
+let c = false;
+console.log(c);
+let d = [12, 13, 15, 18];
+console.log(d);
+let ob = {
+    nom: "john",
+    prenom: "amina",
+    age: 15
+} 
+console.log(ob);
